7c0629d2f8b0e98303666ed27c2bf12344c7329a
MarcoAlv <marcs2805@gmail.com>
2023-11-18 01:54:26 -0500

    Create Estudiante class

---
+++ src/main/java/com/grupo06/Resourses/Estudiante.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.grupo06.Resourses;
+
+/**
+ *
+ * @author Marcos Alvarado
+ */
+public class Estudiante extends Persona {
+    
+    
+    
+}
--- src/main/java/com/grupo06/Resourses/Persona.java
+++ src/main/java/com/grupo06/Resourses/Persona.java
@@ -1,11 +1,46 @@
 package com.grupo06.Resourses;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public abstract class Persona {
-    LocalDate fechaNacimiento;
-    int DNI;
-    String nombre;
-    String apellidos;
+public abstract class Persona implements Serializable{
+    protected LocalDate fechaNacimiento;
+    protected int DNI;
+    protected String nombre;
+    protected String apellidos;
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(int DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    
     
 }
