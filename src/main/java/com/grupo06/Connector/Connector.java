c5691f15dec41b27e9e2263c539a19a145eeae7c
Gabitop42 <sompalominolaredo@gmail.com>
2023-11-23 22:18:21 -0500

    asd

--- src/main/java/com/grupo06/Connector/Connector.java
+++ src/main/java/com/grupo06/Connector/Connector.java
@@ -1,5 +1,6 @@
 package com.grupo06.Connector;
 
+<<<<<<< refs/remotes/origin/Gabo2
 import com.grupo06.Private.Config;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,37 +8,38 @@ import java.sql.SQLException;
 import java.sql.*;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+=======
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+>>>>>>> asd
 
 
 public class Connector {
-    
-    public static DataSource getDataSource(String databaseName){
-        BasicDataSource ds = new BasicDataSource();
-        ds.setUrl(Config.URL + databaseName);
-        ds.setUsername(Config.USERNAME);
-        ds.setPassword(Config.PASSWORD);
-        ds.setInitialSize(5);
-        return ds;
-    }
-    
-    public static Connection getConnection(String databaseName) {
+
+    public Connection getConnection(String databaseName) {
         try {
-            return getDataSource(databaseName).getConnection();
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName,
+                    "root", "1234");
+
+            return connection;
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect to the database!", e);
         }
     }
-    
-    public static void close(ResultSet rs) throws SQLException{
+
+    public static void close(ResultSet rs) throws SQLException {
         rs.close();
     }
-    
-    public static void close(PreparedStatement pstmt) throws SQLException{
+
+    public static void close(PreparedStatement pstmt) throws SQLException {
         pstmt.close();
     }
-    
-    public static void close(Connection conn) throws SQLException{
+
+    public static void close(Connection conn) throws SQLException {
         conn.close();
     }
-    
+
 }
